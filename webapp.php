87ec3f688c459435ccc8fce982fbd7e525257a95
grimksi <thegrimreapir@mail.ru>
2023-04-13 13:50:44 +0500

    13.04

--- webapp.php
+++ webapp.php
@@ -1,5 +1,5 @@
 <?php
 include 'db.php';
-$webAppUrl = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
+$webAppUrl = 'https://webapp.test.iridi.com';
 $approve = pg_fetch_row(pg_query($connection, "SELECT chat_id FROM personals WHERE access_level = 4"))[0];
 ?>
